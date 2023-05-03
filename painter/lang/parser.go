bf2a0142c3e08129ee294114493c717ec34d17c7
Evgen Perveev <perveev.evgen@gmail.com>
2023-05-03 15:06:44 +0300

    Create command parser, solve problem with naming and packages

--- painter/lang/parser.go
+++ painter/lang/parser.go
@@ -1,7 +1,12 @@
 package lang
 
 import (
+	"bufio"
+	"fmt"
+	"image/color"
 	"io"
+	"strconv"
+	"strings"
 
 	"github.com/Valentyn13/Golang_Lab_3v1/painter"
 )
@@ -11,11 +16,68 @@ type Parser struct {
 }
 
 func (p *Parser) Parse(in io.Reader) ([]painter.Operation, error) {
-	var res []painter.Operation
-
+		scanner := bufio.NewScanner(in)
+		scanner.Split(bufio.ScanLines)
 	// TODO: Реалізувати парсинг команд.
-	res = append(res, painter.OperationFunc(painter.WhiteFill))
-	res = append(res, painter.UpdateOp)
+	var result []painter.Operation
+	for scanner.Scan() { // loop through the input stream using the scanner
+		commandLine := scanner.Text()
+oprtn := parse(commandLine) // parse the command line into an operation
+		if oprtn == nil {
+			return nil, fmt.Errorf("Failed to parse this command: %s", commandLine)
+		}
 
-	return res, nil
+		// Replace any previous BgRectangle operation with the new one
+		if bgRect, ok := oprtn.(*painter.BgRectangle); ok {
+			for i, oldOp := range result {
+				if _, ok := oldOp.(*painter.BgRectangle); ok {
+					result[i] = bgRect
+					break
+				}
+			}
+		}
+		result = append(result, oprtn)
+	}
+	return result, nil
+}
+
+
+func parse(commandLine string) painter.Operation {
+	parts := strings.Split(commandLine, " ")
+	instruction := parts[0]
+	var args []string
+	if len(parts) > 1 {
+		args = parts[1:]
+	}
+	var iArgs []int
+	for _, arg := range args {
+		i, err := strconv.Atoi(arg)
+		if err == nil {
+			iArgs = append(iArgs, i)
+		}
+	}
+
+	var figureOps []painter.Figure
+
+	switch instruction {
+	case "white":
+		return painter.OperationFunc(painter.WhiteFill)
+	case "green":
+		return painter.OperationFunc(painter.GreenFill)
+	case "bgrect":
+		return &painter.BgRectangle{X1: iArgs[0], Y1: iArgs[1], X2: iArgs[2], Y2: iArgs[3]}
+	case "figure":
+		clr := color.RGBA{R: 255, G: 255, B: 0, A: 1}
+		figure := painter.Figure{X: iArgs[0], Y: iArgs[1], C: clr}
+		figureOps = append(figureOps, figure)
+		return &figure
+	case "move":
+		return &painter.Move{X: iArgs[0], Y: iArgs[1], Figures: figureOps}
+	case "reset":
+		figureOps = figureOps[0:0]
+		return painter.OperationFunc(painter.ClearScreen)
+	case "update":
+		return painter.UpdateOp
+	}
+	return nil
 }
